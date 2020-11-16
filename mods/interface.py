2227f636d181cbd0d80bfcc2910d2af775dcee8a
greyhatlinux <vishalgorai09@gmail.com>
2020-11-16 20:46:35 +0530

    added at input field

--- mods/interface.py
+++ mods/interface.py
@@ -27,29 +27,28 @@ def ui():
 
 
     def show():
-        label = tk.Label(root, text="From: " + str(from_var.get()) + " To: " + str(to_var.get())).pack()
+        label = tk.Label(root, text="From: " + str(from_var.get()) + " To: " + str(to_var.get()) + ", at: " + str(at_var.get())).pack()
 
     def howToPopup():
-        tkmsg.showinfo(title="How to Guide is here!", message="1. Fill the 'from' station \n2. Fill the 'to' station \n3. Click the 'Submit' button")
+        tkmsg.showinfo(title="How to Guide is here!", message="1. Fill the 'from' station \n2. Fill the 'to' station \n3. Fill the 'at' section in 24 hour format.\nEx: 14:10, if it is 2:10 pm\n4. Click the 'Submit' button")
 
     from_var = tk.StringVar()
     from_var.set("A")
-
     from_station = tk.OptionMenu(root, from_var, "A", "B", "C", "D", "E")
     from_station.pack()
-
     from_label = tk.Label(root, text="From", pady=10).pack()
 
     to_var = tk.StringVar()
     to_var.set("A")
-
     to_station = tk.OptionMenu(root, to_var, "A", "B", "C", "D", "E")
     to_station.pack()
-
     to_label = tk.Label(root, text="To", pady=10).pack()
 
+    at_var = tk.Entry(root, width=10)
+    at_var.pack()
+    at_label = tk.Label(root, text="At", pady=10).pack()
     
-    submit_btn = tk.Button(root, text="Show selection", command=show)
+    submit_btn = tk.Button(root, text="Submit", command=show)
     submit_btn.pack()
 
     howTo_btn = tk.Button(root, text="How to use this window", pady=15, command=howToPopup)
