6e2a1383345e6d1bab01ccde1625ae07c15e117f
greyhatlinux <vishalgorai09@gmail.com>
2020-11-16 20:16:28 +0530

    added from-to and how-to guide section

--- mods/interface.py
+++ mods/interface.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as tkmsg
 import datetime as dt
 
 def ui():
@@ -12,6 +13,36 @@ def ui():
     date = "Time Now: " + str(dt.datetime.now())
     myLabel = tk.Label(root, text=date)
     myLabel.pack()
-    btn = tk.Button(root, text="Close", width=10, command=root.destroy)
+
+    def show():
+        label = tk.Label(root, text="From: " + str(from_var.get()) + " To: " + str(to_var.get())).pack()
+
+    def howToPopup():
+        tkmsg.showinfo(title="How to Guide is here!", message="1. Fill the 'from' station \n2. Fill the 'to' station \n3. Click the 'Submit' button")
+
+    from_var = tk.StringVar()
+    from_var.set("A")
+
+    from_station = tk.OptionMenu(root, from_var, "A", "B", "C", "D", "E")
+    from_station.pack()
+
+    from_label = tk.Label(root, text="From", pady=10).pack()
+
+    to_var = tk.StringVar()
+    to_var.set("A")
+
+    to_station = tk.OptionMenu(root, to_var, "A", "B", "C", "D", "E")
+    to_station.pack()
+
+    to_label = tk.Label(root, text="To", pady=10).pack()
+
+    
+    submit_btn = tk.Button(root, text="Show selection", command=show)
+    submit_btn.pack()
+
+    howTo_btn = tk.Button(root, text="How to use this window", pady=15, command=howToPopup)
+    howTo_btn.pack()
+
+    btn = tk.Button(root, text="Close", width=10, command=root.quit)
     btn.pack()
     root.mainloop()
